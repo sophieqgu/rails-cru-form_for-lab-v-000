7470f12273f1771ac8cabd1906bd1495382192d5
Sophie Gu <sophieqgu@gmail.com>
2020-11-17 04:16:11 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201117034503) do
+ActiveRecord::Schema.define(version: 20201117041549) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20201117034503) do
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "genre_id"
   end
 
 end
